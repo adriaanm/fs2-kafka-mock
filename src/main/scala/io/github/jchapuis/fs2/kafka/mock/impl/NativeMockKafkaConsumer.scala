2f893724d8dadef6f5122054501a707aca1342bf
Adriaan Moors <adriaanm@gmail.com>
2024-08-21 14:57:11 +0200

    callback hell :rubber-duck:

--- src/main/scala/io/github/jchapuis/fs2/kafka/mock/impl/NativeMockKafkaConsumer.scala
+++ src/main/scala/io/github/jchapuis/fs2/kafka/mock/impl/NativeMockKafkaConsumer.scala
@@ -19,19 +19,25 @@ import scala.jdk.CollectionConverters.*
 import scala.collection.concurrent.TrieMap
 import cats.effect.unsafe.IORuntime
 
+// the mockConsumer is not thread safe (also, all its methods are synchronized, so be careful with deadlocks),
+// should only interact with it by scheduling tasks using schedulePollTask
+// the poll method is where it's all happening: it will be called by the stream consuming from this mocked consumer,
+// and by scheduling a task to run during `poll`, we can add records to the consumer to be returned by the poll
 private[mock] class NativeMockKafkaConsumer(
-  val mockConsumer: MockConsumer[Array[Byte], Array[Byte]],
+  mockConsumer: MockConsumer[Array[Byte], Array[Byte]],
   topics: Seq[String]
 )(implicit runtime: IORuntime)
   extends MockKafkaConsumer {
+  private final val singlePartition = 0
 
-  {
-    val partitions       = topics.map(topic => new TopicPartition(topic, 0))
+  // configure start offsets and make sure we are assigned to the partitions
+  mockConsumer.schedulePollTask { () =>
+    val partitions       = topics.map(topic => new TopicPartition(topic, singlePartition))
     val beginningOffsets = partitions.map(_ -> (0L: java.lang.Long)).toMap
     mockConsumer.updateBeginningOffsets(beginningOffsets.asJava)
-  }
 
-  private val singlePartition = 0
+    mockConsumer.rebalance(topics.map(new TopicPartition(_, singlePartition)).asJava)
+  }
 
   // create a java.util.concurrent.ConcurrentMap from topics to 0
   private val currentOffsets = TrieMap(topics.map(_ -> 0L): _*)
@@ -40,11 +46,9 @@ private[mock] class NativeMockKafkaConsumer(
     keySerializer: KeySerializer[IO, K],
     valueSerializer: ValueSerializer[IO, V]
   ): IO[Unit] = for {
-    _     <- IO.println(s"publishing to topic $topic under key $key with value $value")
     key   <- keySerializer.serialize(topic, Headers.empty, key)
     value <- valueSerializer.serialize(topic, Headers.empty, value)
-    _     <- IO(addRecord(topic, key, Option(value), timestamp))
-    _     <- IO.sleep(500.milli) // TODO: why is this needed?? cede before and after doesn't do anything
+    pf    <- IO(addRecord(topic, key, Option(value), timestamp))
   } yield ()
 
   private def addRecord(
@@ -73,10 +77,9 @@ private[mock] class NativeMockKafkaConsumer(
       Optional.empty[Integer]
     )
 
-    println(s"scheduling record to topic $topic at offset $offset")
-
+    // println(s"scheduling task for poll to return record in $topic at offset $offset")
     mockConsumer.schedulePollTask { () =>
-      println(s"adding record to topic $topic at offset $offset")
+      // println(s"adding record to topic $topic at offset $offset")
       mockConsumer.addRecord(record)
     }
   }
@@ -87,120 +90,98 @@ private[mock] class NativeMockKafkaConsumer(
       _   <- IO(addRecord(topic, key, None, None))
     } yield ()
 
-  private def withMutex[T](f: => T): T = f // synchronized(f)
-
   @nowarn("cat=deprecation")
   implicit lazy val mkConsumer: MkConsumer[IO] = new MkConsumer[IO] {
     private val mockFacadeWithPresetSubscriptions = new KafkaByteConsumer {
-      def assignment(): util.Set[TopicPartition] = withMutex(mockConsumer.assignment())
+      def assignment(): util.Set[TopicPartition] = mockConsumer.assignment()
 
-      def subscription(): util.Set[String] = withMutex(mockConsumer.subscription())
+      def subscription(): util.Set[String] = mockConsumer.subscription()
 
-      def subscribe(topics: util.Collection[String]): Unit = withMutex {
-        println(s"subscribing to $topics")
-        mockConsumer.subscribe(topics)
-        ensureConsumerAssignedTo(topics.asScala.toList)
-      }
+      def subscribe(topics: util.Collection[String]): Unit = mockConsumer.subscribe(topics)
 
-      def subscribe(topics: util.Collection[String], callback: ConsumerRebalanceListener): Unit = withMutex {
+      def subscribe(topics: util.Collection[String], callback: ConsumerRebalanceListener): Unit =
         mockConsumer.subscribe(topics, callback)
-        ensureConsumerAssignedTo(topics.asScala.toList)
-      }
 
-      def assign(partitions: util.Collection[TopicPartition]): Unit = withMutex(mockConsumer.assign(partitions))
+      def assign(partitions: util.Collection[TopicPartition]): Unit = mockConsumer.assign(partitions)
 
       def subscribe(pattern: Pattern, callback: ConsumerRebalanceListener): Unit =
-        withMutex(mockConsumer.subscribe(pattern, callback))
+        mockConsumer.subscribe(pattern, callback)
 
-      def subscribe(pattern: Pattern): Unit = withMutex(mockConsumer.subscribe(pattern))
+      def subscribe(pattern: Pattern): Unit = mockConsumer.subscribe(pattern)
 
-      def unsubscribe(): Unit = withMutex(mockConsumer.unsubscribe())
+      def unsubscribe(): Unit = mockConsumer.unsubscribe()
 
-      def poll(timeout: Long): ConsumerRecords[Array[Byte], Array[Byte]] = withMutex(mockConsumer.poll(timeout))
+      def poll(timeout: Long): ConsumerRecords[Array[Byte], Array[Byte]] = mockConsumer.poll(timeout)
 
-      def poll(timeout: Duration): ConsumerRecords[Array[Byte], Array[Byte]] = withMutex {
-        val res = mockConsumer.poll(timeout)
-        println(s"polling result: ${res.iterator().asScala.toList}")
-        res
-      }
+      def poll(timeout: Duration): ConsumerRecords[Array[Byte], Array[Byte]] = mockConsumer.poll(timeout)
 
-      def commitSync(): Unit = withMutex(mockConsumer.commitSync())
+      def commitSync(): Unit = mockConsumer.commitSync()
 
-      def commitSync(timeout: Duration): Unit = withMutex(mockConsumer.commitSync(timeout))
+      def commitSync(timeout: Duration): Unit = mockConsumer.commitSync(timeout)
 
-      def commitSync(offsets: util.Map[TopicPartition, OffsetAndMetadata]): Unit = withMutex(
-        mockConsumer.commitSync(offsets)
-      )
+      def commitSync(offsets: util.Map[TopicPartition, OffsetAndMetadata]): Unit = mockConsumer.commitSync(offsets)
 
       def commitSync(offsets: util.Map[TopicPartition, OffsetAndMetadata], timeout: Duration): Unit =
-        withMutex(mockConsumer.commitSync(offsets, timeout))
+        mockConsumer.commitSync(offsets, timeout)
 
-      def commitAsync(): Unit = withMutex(mockConsumer.commitAsync())
+      def commitAsync(): Unit = mockConsumer.commitAsync()
 
-      def commitAsync(callback: OffsetCommitCallback): Unit = withMutex(mockConsumer.commitAsync(callback))
+      def commitAsync(callback: OffsetCommitCallback): Unit = mockConsumer.commitAsync(callback)
 
       def commitAsync(offsets: util.Map[TopicPartition, OffsetAndMetadata], callback: OffsetCommitCallback): Unit =
-        withMutex(mockConsumer.commitAsync(offsets, callback))
+        mockConsumer.commitAsync(offsets, callback)
 
-      def seek(partition: TopicPartition, offset: Long): Unit = withMutex(mockConsumer.seek(partition, offset))
+      def seek(partition: TopicPartition, offset: Long): Unit = mockConsumer.seek(partition, offset)
 
       def seek(partition: TopicPartition, offsetAndMetadata: OffsetAndMetadata): Unit =
-        withMutex(mockConsumer.seek(partition, offsetAndMetadata))
+        mockConsumer.seek(partition, offsetAndMetadata)
 
-      def seekToBeginning(partitions: util.Collection[TopicPartition]): Unit =
-        withMutex(mockConsumer.seekToBeginning(partitions))
+      def seekToBeginning(partitions: util.Collection[TopicPartition]): Unit = mockConsumer.seekToBeginning(partitions)
 
-      def seekToEnd(partitions: util.Collection[TopicPartition]): Unit = withMutex(
-        mockConsumer.seekToEnd(partitions)
-      )
+      def seekToEnd(partitions: util.Collection[TopicPartition]): Unit = mockConsumer.seekToEnd(partitions)
 
-      def position(partition: TopicPartition): Long = withMutex(mockConsumer.position(partition))
+      def position(partition: TopicPartition): Long = mockConsumer.position(partition)
 
-      def position(partition: TopicPartition, timeout: Duration): Long = withMutex(
-        mockConsumer.position(partition, timeout)
-      )
+      def position(partition: TopicPartition, timeout: Duration): Long = mockConsumer.position(partition, timeout)
 
-      def committed(partition: TopicPartition): OffsetAndMetadata = withMutex(mockConsumer.committed(partition))
+      def committed(partition: TopicPartition): OffsetAndMetadata = mockConsumer.committed(partition)
 
       def committed(partition: TopicPartition, timeout: Duration): OffsetAndMetadata =
-        withMutex(mockConsumer.committed(partition, timeout))
+        mockConsumer.committed(partition, timeout)
 
       def committed(partitions: util.Set[TopicPartition]): util.Map[TopicPartition, OffsetAndMetadata] =
-        withMutex(mockConsumer.committed(partitions))
+        mockConsumer.committed(partitions)
 
       def committed(
         partitions: util.Set[TopicPartition],
         timeout: Duration
-      ): util.Map[TopicPartition, OffsetAndMetadata] = withMutex(mockConsumer.committed(partitions, timeout))
+      ): util.Map[TopicPartition, OffsetAndMetadata] = mockConsumer.committed(partitions, timeout)
 
-      def metrics(): util.Map[MetricName, _ <: Metric] = withMutex(mockConsumer.metrics())
+      def metrics(): util.Map[MetricName, _ <: Metric] = mockConsumer.metrics()
 
+      // TODO: probably not needed
       def partitionsFor(topic: String): util.List[PartitionInfo] =
-        withMutex {
-          if (mockConsumer.assignment().isEmpty) {
-            util.List.of(new PartitionInfo(topic, singlePartition, null, null, null))
-          } else {
-            mockConsumer.partitionsFor(topic)
-          }
+        if (mockConsumer.assignment().isEmpty) {
+          util.List.of(new PartitionInfo(topic, singlePartition, null, null, null))
+        } else {
+          mockConsumer.partitionsFor(topic)
         }
 
       def partitionsFor(topic: String, timeout: Duration): util.List[PartitionInfo] = partitionsFor(topic)
 
-      def listTopics(): util.Map[String, util.List[PartitionInfo]] = withMutex(mockConsumer.listTopics())
+      def listTopics(): util.Map[String, util.List[PartitionInfo]] = mockConsumer.listTopics()
 
-      def listTopics(timeout: Duration): util.Map[String, util.List[PartitionInfo]] = withMutex(
-        mockConsumer.listTopics(timeout)
-      )
+      def listTopics(timeout: Duration): util.Map[String, util.List[PartitionInfo]] = mockConsumer.listTopics(timeout)
 
-      def paused(): util.Set[TopicPartition] = withMutex(mockConsumer.paused())
+      def paused(): util.Set[TopicPartition] = mockConsumer.paused()
 
-      def pause(partitions: util.Collection[TopicPartition]): Unit = withMutex(mockConsumer.pause(partitions))
+      def pause(partitions: util.Collection[TopicPartition]): Unit = mockConsumer.pause(partitions)
 
-      def resume(partitions: util.Collection[TopicPartition]): Unit = withMutex(mockConsumer.resume(partitions))
+      def resume(partitions: util.Collection[TopicPartition]): Unit = mockConsumer.resume(partitions)
 
       def offsetsForTimes(
         timestampsToSearch: util.Map[TopicPartition, lang.Long]
-      ): util.Map[TopicPartition, OffsetAndTimestamp] = withMutex {
+      ): util.Map[TopicPartition, OffsetAndTimestamp] = {
         val partitions = timestampsToSearch.keySet().asScala.toList
         mockConsumer
           .beginningOffsets(
@@ -219,42 +200,37 @@ private[mock] class NativeMockKafkaConsumer(
       ): util.Map[TopicPartition, OffsetAndTimestamp] = offsetsForTimes(timestampsToSearch)
 
       def beginningOffsets(partitions: util.Collection[TopicPartition]): util.Map[TopicPartition, lang.Long] =
-        withMutex(mockConsumer.beginningOffsets(partitions))
+        mockConsumer.beginningOffsets(partitions)
 
       def beginningOffsets(
         partitions: util.Collection[TopicPartition],
         timeout: Duration
-      ): util.Map[TopicPartition, lang.Long] = withMutex(mockConsumer.beginningOffsets(partitions, timeout))
+      ): util.Map[TopicPartition, lang.Long] = mockConsumer.beginningOffsets(partitions, timeout)
 
       def endOffsets(partitions: util.Collection[TopicPartition]): util.Map[TopicPartition, lang.Long] =
-        withMutex(mockConsumer.endOffsets(partitions))
+        mockConsumer.endOffsets(partitions)
 
       def endOffsets(
         partitions: util.Collection[TopicPartition],
         timeout: Duration
-      ): util.Map[TopicPartition, lang.Long] = withMutex(mockConsumer.endOffsets(partitions, timeout))
+      ): util.Map[TopicPartition, lang.Long] = mockConsumer.endOffsets(partitions, timeout)
 
-      def currentLag(topicPartition: TopicPartition): OptionalLong = withMutex(
-        mockConsumer.currentLag(topicPartition)
-      )
+      def currentLag(topicPartition: TopicPartition): OptionalLong = mockConsumer.currentLag(topicPartition)
 
-      def groupMetadata(): ConsumerGroupMetadata = withMutex(mockConsumer.groupMetadata())
+      def groupMetadata(): ConsumerGroupMetadata = mockConsumer.groupMetadata()
 
-      def enforceRebalance(): Unit = withMutex(mockConsumer.enforceRebalance())
+      def enforceRebalance(): Unit = mockConsumer.enforceRebalance()
 
-      def enforceRebalance(reason: String): Unit = withMutex(mockConsumer.enforceRebalance(reason))
+      def enforceRebalance(reason: String): Unit = mockConsumer.enforceRebalance(reason)
 
-      def close(): Unit = withMutex(mockConsumer.close())
+      def close(): Unit = mockConsumer.close()
 
-      def close(timeout: Duration): Unit = withMutex(mockConsumer.close(timeout))
+      def close(timeout: Duration): Unit = mockConsumer.close(timeout)
 
-      def wakeup(): Unit = withMutex(mockConsumer.wakeup())
+      def wakeup(): Unit = mockConsumer.wakeup()
 
       def clientInstanceId(timeout: Duration): Uuid = Uuid.randomUuid()
     }
-
-    private def ensureConsumerAssignedTo(topics: List[String]): Unit =
-      mockConsumer.rebalance(topics.map(new TopicPartition(_, singlePartition)).asJava)
 
     def apply[G[_]](settings: ConsumerSettings[G, ?, ?]): IO[KafkaByteConsumer] =
       IO.pure(mockFacadeWithPresetSubscriptions)
